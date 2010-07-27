297b33d92ed496269624015fe1e693baa466350e
terrence.a.pietrondi <terrence.a.pietrondi@0cbc4ca5-bce1-8640-bc7c-eca00719f28b>
2010-07-27 13:31:45 +0000

    formalizing setup to methods for duplicate setup

--- trunk/three-legged-dance.py
+++ trunk/three-legged-dance.py
@@ -6,6 +6,7 @@ import oauth2 as oauth
 import httplib2
 import urllib
 import simplejson
+import logging
 
 __version__ = '0.1'
 
@@ -15,150 +16,116 @@ except ImportError:
     from cgi import parse_qs, parse_qsl
 
 headers = {
-    'user-agent': 'gdata-python-v3-sample-client/%s' % __version__,
+    'user-agent': 'gmail-location-for-latitude/%s' % __version__,
     'content-type': 'application/x-www-form-urlencoded'
     }
 
 consumer_key = 'gmail-location-for-latitude.appspot.com'
 consumer_secret = 'cflcYnKXy0Hj/97TpaF9VA4U'
 
-request_token_url = 'https://www.google.com/accounts/OAuthGetRequestToken?domain=gmail-location-for-latitude.appspot.com&scope=https://www.googleapis.com/auth/latitude'
-authorize_url = 'https://www.google.com/latitude/apps/OAuthAuthorizeToken?domain=gmail-location-for-latitude.appspot.com&scope=https://www.googleapis.com/auth/latitude'
+request_token_url_lat = 'https://www.google.com/accounts/OAuthGetRequestToken?domain=gmail-location-for-latitude.appspot.com&scope=https://www.googleapis.com/auth/latitude'
+authorize_url_lat = 'https://www.google.com/latitude/apps/OAuthAuthorizeToken?domain=gmail-location-for-latitude.appspot.com&scope=https://www.googleapis.com/auth/latitude'
 access_token_url = 'https://www.google.com/accounts/OAuthGetAccessToken'
 
 request_token_url_gmail = 'https://www.google.com/accounts/OAuthGetRequestToken?domain=gmail-location-for-latitude.appspot.com&scope=https://mail.google.com/mail/feed/atom/'
 authorize_url_gmail = 'https://www.google.com/accounts/OAuthAuthorizeToken?domain=gmail-location-for-latitude.appspot.com&scope=https://mail.google.com/mail/feed/atom/'
 
-consumer = oauth.Consumer(consumer_key, consumer_secret)
-client = oauth.Client(consumer)
-
-# Step 1: Get a request token. This is a temporary token that is used for
-# having the user authorize an access token and to sign the request to obtain
-# said access token.
 parameters = {
-  'xoauth_displayname': 'gmail-location-for-latitude',
-  'oauth_callback': 'oob'
-    }
+	  'xoauth_displayname': 'gmail-location-for-latitude',
+	  'oauth_callback': 'oob'
+		}
 
-resp, content = client.request(request_token_url, 'POST', headers=headers,
-    body=urllib.urlencode(parameters, True))
-    
-resp_gmail, content_gmail = client.request(request_token_url_gmail, 'POST', headers=headers,
-    body=urllib.urlencode(parameters, True))
-    
-if resp['status'] != '200':
-  print content
-  raise Exception('Invalid response %s.' % resp['status'])
+def step1_get_request_token(client,request_token_url):
+	# Step 1: Get a request token. This is a temporary token that is used for
+	# having the user authorize an access token and to sign the request to obtain
+	# said access token.
+	
+	resp, content = client.request(request_token_url, 'POST', headers=headers,
+    	body=urllib.urlencode(parameters, True))
+   
+	if resp['status'] != '200':
+  		print content
+  		raise Exception('Invalid response %s.' % resp['status'])
   
-if resp_gmail['status'] != '200':
-  print content
-  raise Exception('Invalid response %s.' % resp['status'])
+	request_token = dict(parse_qsl(content))
+	
+	return request_token
 
-request_token = dict(parse_qsl(content))
-request_token_gmail = dict(parse_qsl(content_gmail))
+def step2_redirect_to_provider(request_token, authorize_url):
 
-print 'Request Token:'
-print '    - oauth_token        = %s' % request_token['oauth_token']
-print '    - oauth_token_secret = %s' % request_token['oauth_token_secret']
-print
-print '    - oauth_token        = %s' % request_token_gmail['oauth_token']
-print '    - oauth_token_secret = %s' % request_token_gmail['oauth_token_secret']
-print
+	base_url = urlparse.urlparse(authorize_url)
+	query = parse_qs(base_url.query)
+	query['oauth_token'] = request_token['oauth_token']
 
-# Step 2: Redirect to the provider. Since this is a CLI script we do not
-# redirect. In a web application you would redirect the user to the URL
-# below.
+	url = (base_url.scheme, base_url.netloc, base_url.path, base_url.params,urllib.urlencode(query, True), base_url.fragment)
+          
+	authorize_url = urlparse.urlunparse(url)
+	
+	return authorize_url
 
-base_url = urlparse.urlparse(authorize_url)
-query = parse_qs(base_url.query)
-query['oauth_token'] = request_token['oauth_token']
+def step3_sign_request(client,access_token_url,oauth_verifier,request_token):
+	# Step 3: Once the consumer has redirected the user back to the oauth_callback
+	# URL you can request the access token the user has approved. You use the
+	# request token to sign this request. After this is done you throw away the
+	# request token and use the access token returned. You should store this
+	# access token somewhere safe, like a database, for future use.
+	token = oauth.Token(request_token['oauth_token'],
+		request_token['oauth_token_secret'])
+	token.set_verifier(oauth_verifier)
+	client = oauth.Client(consumer, token)
+	
+	resp, content = client.request(access_token_url, 'POST', headers=headers,
+		body=urllib.urlencode(parameters, True))
+	access_token = dict(parse_qsl(content))
+	
+	return access_token
 
-base_url_gmail = urlparse.urlparse(authorize_url_gmail)
-query_gmail = parse_qs(base_url_gmail.query)
-query_gmail['oauth_token'] = request_token_gmail['oauth_token']
+def write_access_token_to_file(filename,d):
+	f = open(filename, 'w')
+	f.write(simplejson.dumps(d))
+	f.close()
 
-print urllib.urlencode(query, True)
-print urllib.urlencode(query_gmail, True)
+if __name__ == '__main__':
+	consumer = oauth.Consumer(consumer_key, consumer_secret)
+	client = oauth.Client(consumer)
+	
+	token_lat = step1_get_request_token(client,request_token_url_lat)
+	token_gmail = step1_get_request_token(client,request_token_url_gmail)
+	
+	auth_url_lat = step2_redirect_to_provider(token_lat,authorize_url_lat)
+	auth_url_gmail = step2_redirect_to_provider(token_gmail,authorize_url_gmail)
+	
+	print
+	print 'visit the following URLs in your browser and copy the PIN per site'
+	print
+	print 'google latitude: %s' % auth_url_lat
+	print
+	print 'gmail: %s' % auth_url_gmail
+	print
+	
+	accepted = 'n'
+	while accepted.lower() == 'n':
+		accepted = raw_input('Have you authorized me? (y/n) ')
+	
+	oauth_verifier_lat = raw_input('What is the PIN? (latitude)')
+	oauth_verifier_gmail = raw_input('What is the PIN? (gmail)')
 
-url = (base_url.scheme, base_url.netloc, base_url.path, base_url.params,
-       urllib.urlencode(query, True), base_url.fragment)
-       
-url_gmail = (base_url_gmail.scheme, base_url_gmail.netloc, base_url_gmail.path, base_url_gmail.params,
-       urllib.urlencode(query_gmail, True), base_url_gmail.fragment)
-       
-authorize_url = urlparse.urlunparse(url)
-authorize_url_gmail = urlparse.urlunparse(url_gmail)
-
-print 'Go to the following link in your browser:'
-print authorize_url
-print
-print authorize_url_gmail
-print
-
-
-# After the user has granted access to you, the consumer, the provider will
-# redirect you to whatever URL you have told them to redirect to. You can
-# usually define this in the oauth_callback argument as well.
-accepted = 'n'
-while accepted.lower() == 'n':
-    accepted = raw_input('Have you authorized me? (y/n) ')
-oauth_verifier = raw_input('What is the PIN? ')
-oauth_verifier_gmail = raw_input('What is the PIN? (gmail)')
-
-# Step 3: Once the consumer has redirected the user back to the oauth_callback
-# URL you can request the access token the user has approved. You use the
-# request token to sign this request. After this is done you throw away the
-# request token and use the access token returned. You should store this
-# access token somewhere safe, like a database, for future use.
-token = oauth.Token(request_token['oauth_token'],
-    request_token['oauth_token_secret'])
-token.set_verifier(oauth_verifier)
-client = oauth.Client(consumer, token)
-
-token_gmail = oauth.Token(request_token_gmail['oauth_token'],
-    request_token_gmail['oauth_token_secret'])
-token_gmail.set_verifier(oauth_verifier_gmail)
-
-client_gmail = oauth.Client(consumer, token_gmail)
-
-resp, content = client.request(access_token_url, 'POST', headers=headers,
-    body=urllib.urlencode(parameters, True))
-access_token = dict(parse_qsl(content))
-
-print access_token
-
-resp_gmail, content_gmail = client_gmail.request(access_token_url, 'POST', headers=headers,
-    body=urllib.urlencode(parameters, True))
-access_token_gmail = dict(parse_qsl(content_gmail))
-
-print access_token_gmail
-
-print 'Access Token:'
-print '    - oauth_token        = %s' % access_token['oauth_token']
-print '    - oauth_token_secret = %s' % access_token['oauth_token_secret']
-print
-print '    - oauth_token        = %s' % access_token_gmail['oauth_token']
-print '    - oauth_token_secret = %s' % access_token_gmail['oauth_token_secret']
-print
-print 'You may now access protected resources using the access tokens above.'
-print
-
-d = dict(
-  consumer_key = consumer_key,
-  consumer_secret = consumer_secret
-    )
-d.update(access_token)
-
-f = open('oauth_token.dat', 'w')
-f.write(simplejson.dumps(d))
-f.close()
-
-d_gmail = dict(
-  consumer_key = consumer_key,
-  consumer_secret = consumer_secret
-    )
-d_gmail.update(access_token_gmail)
-
-f = open('oauth_token_gmail.dat', 'w')
-f.write(simplejson.dumps(d_gmail	))
-f.close()
+	access_token_lat = step3_sign_request(client,access_token_url,oauth_verifier_lat,token_lat)
+	access_token_gmail = step3_sign_request(client,access_token_url,oauth_verifier_gmail,token_gmail)
+	
+	d = dict(
+  		consumer_key = consumer_key,
+  		consumer_secret = consumer_secret)
+	
+	d.update(access_token_gmail)
+	
+	write_access_token_to_file('oauth_token_gmail.dat',d)
+	
+	d = dict(
+  		consumer_key = consumer_key,
+  		consumer_secret = consumer_secret)
+	
+	d.update(access_token_lat)
+	
+	write_access_token_to_file('oauth_token_lat.dat',d)
+	
