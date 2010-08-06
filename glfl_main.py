2442d9fa6771380a77302a4c17879b345176bc7f
terrence.a.pietrondi@gmail.com <terrence.a.pietrondi@gmail.com@0cbc4ca5-bce1-8640-bc7c-eca00719f28b>
2010-08-06 12:29:55 +0000

    got template working

--- glfl_main.py
+++ glfl_main.py
@@ -1,32 +1,38 @@
-#from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
-#from google.appengine.ext import db
+from google.appengine.ext import db
+from google.appengine.api import users
 
-#import cgi,os,logging,sys
+import cgi,os,logging,sys
 
-#class Preferences(db.Model):
-#	who = db.UserPropery(required=True)
-#	latitude_token = db.StringProperty(required=True)
-#	gmail_token = db.StringProperty(required=True)
-#	trusted_from = db.StringProperty(required=False)
-#	created = db.DateTimeProperty(auto_now_add=True)
-#	updated = db.DateTimeProperty(auto_now=True)
+class Preferences(db.Model):
+	who = db.UserProperty(required=True)
+	latitude_token = db.StringProperty(required=False)
+	gmail_token = db.StringProperty(required=False)
+	trusted_from = db.StringProperty(required=False)
+	created = db.DateTimeProperty(auto_now_add=True)
+	updated = db.DateTimeProperty(auto_now=True)
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
-		pass
-		#user_preferences_query = db.GqlQuery("SELECT * FROM Preferences WHERE owner = :1", users.get_current_user())
-		#results = user_preferences_query.fetch(1)
+		user_preferences_query = db.GqlQuery("SELECT * FROM Preferences WHERE owner = :1", users.get_current_user())
+		results = user_preferences_query.fetch(1)
 		
-		#preference_model = None
+		preference_model = None
 		
-		#if len(ressults) == 1: for result in results: preference_model = result
+		if len(results) == 1: 
+			for result in results: 
+				preference_model = result
 		
-		#_template_values = {'preferences' : preference_model}
+		else:
+			preference_model = Preferences(who= users.get_current_user())
+			preference_model.put()
 		
-		#_path = os.path.join(os.path.dirname(__file__), 'Template_MainPage.html')
-		#self.response.out.write(template.render(_path, _template_values))
+		_template_values = {'preferences' : preference_model}
+		
+		_path = os.path.join(os.path.dirname(__file__), 'Template_MainPage.html')
+		self.response.out.write(template.render(_path, _template_values))
 
 application = webapp.WSGIApplication([('/', MainPage)],debug=True)
 
